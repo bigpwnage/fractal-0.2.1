51873b1bf23b28ce00ef0422b6fe6972874c2aca
TheMan <106723200+bigpwnage@users.noreply.github.com>
2024-09-15 19:25:56 -0700

    added line of code for error

--- src/support/lockedpool.cpp
+++ src/support/lockedpool.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2016-2020 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
+#include <stdexcept>
 #include <support/lockedpool.h>
 #include <support/cleanse.h>
 
